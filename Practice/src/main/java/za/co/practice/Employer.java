1b4aa22c6798a013fd904f3f4f51ec83f5729fa9
kamashijhb024 <kamashijhb024@student.wethinkcode.co.za>
2025-05-14 11:41:53 +0200

    added another question

--- Practice/src/main/java/za/co/practice/Employer.java
+++ Practice/src/main/java/za/co/practice/Employer.java
@@ -28,6 +28,30 @@ public class Employer {
     public String getGender() {
         return gender;
     }
+    /*
+
+    add a method that generates unique id for the user
+    by combining name and surname and having the age at the end
+    e.g.
+        name = John
+        surname  = Wick
+        Age = 32
+        Gender = Male
+        id = johwick32
+
+    -Advance form
+        make another that generates a id
+        using first 3 letters of the name
+        the first 3 letters of the suname
+        age and capital letter of the first letter of their Gender
+        e.g.
+            name = John
+            surname  = Wick
+            Age = 32
+            Gender = Male
+            id = johwic32M
+     */
+
 
 
 }
