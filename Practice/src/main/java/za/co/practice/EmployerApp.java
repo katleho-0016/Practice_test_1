bfed1975188d34abbfb26705c26702d909b0fd5b
kamashijhb024 <kamashijhb024@student.wethinkcode.co.za>
2025-05-14 11:45:54 +0200

    added another question

---
+++ Practice/src/main/java/za/co/practice/EmployerApp.java
@@ -0,0 +1,9 @@
+package za.co.practice;
+
+public class EmployerApp {
+    /*
+        Make a main method
+        initialize 3 new employers
+        print out who is the olders among them (write code that checks)
+     */
+}
